16c5acf6f52fed0562553bcdb6e54030dc2f5018
Manuel Quarneti <manuq01@pm.me>
2023-09-29 18:55:28 +0200

    🚧 WIP

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 // SPDX-FileCopyrightText: 2023 Manuel Quarneti <manuel.quarneti@proton.me>
 // SPDX-License-Identifier: GPL-2.0-only
 
+use crate::types::app::App;
+
 mod wbfs_file;
-
-use std::path::PathBuf;
-
-use eframe::egui;
-use sysinfo::{DiskExt, System, SystemExt};
+mod types;
 
 fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions::default();
@@ -15,54 +13,4 @@ fn main() -> eframe::Result<()> {
         native_options,
         Box::new(|cc| Box::new(App::new(cc))),
     )
-}
-
-#[derive(Default)]
-struct App {
-    sys: System,
-    current_disk: Option<PathBuf>,
-}
-
-impl App {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        let mut sys = System::new_all();
-        sys.refresh_disks_list();
-        sys.refresh_disks();
-
-        Self {
-            sys,
-            current_disk: None,
-        }
-    }
-}
-
-impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        std::process::exit(0);
-                    }
-                });
-
-                ui.menu_button("Disks", |ui| {
-                    for disk in self.sys.disks().iter().filter(|disk| disk.is_removable()) {
-                        if ui.button(disk.name().to_str().unwrap()).clicked() {
-                            self.current_disk = Some(disk.mount_point().to_path_buf());
-                        }
-                    }
-                });
-            });
-        });
-
-        if let Some(disk) = &self.current_disk {
-            egui::TopBottomPanel::bottom("disk_info").show(ctx, |ui| {
-                ui.horizontal(|ui| {
-                    ui.label("Disk:");
-                    ui.label(disk.to_str().unwrap());
-                });
-            });
-        }
-    }
 }
---
+++ src/types/mod.rs
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2023 Manuel Quarneti <manuel.quarneti@proton.me>
+// SPDX-License-Identifier: GPL-2.0-only
+
+pub mod app;
+pub mod drive;
+pub mod game;
