5bdea110466611cb136cf31d43acb2e00a7d583f
Manuel Quarneti <manuq01@pm.me>
2023-10-04 11:36:22 +0200

    feat: Check for updates

---
+++ src/updater.rs
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2023 Manuel Quarneti <manuel.quarneti@proton.me>
+// SPDX-License-Identifier: GPL-2.0-only
+
+use anyhow::Result;
+use rfd::{MessageDialog, MessageDialogResult};
+use serde::Deserialize;
+
+const LATEST_RELEASE_URL: &str = "https://api.github.com/repos/mq1/TinyWiiBackupManager/releases/latest";
+const RELEASES_BASE_URL: &str = "https://github.com/mq1/TinyWiiBackupManager/releases/tag/";
+
+#[derive(Deserialize)]
+struct Release {
+    tag_name: String,
+}
+
+pub fn check_for_updates() -> Result<()> {
+    let latest_release = ureq::get(LATEST_RELEASE_URL).call()?.into_json::<Release>()?.tag_name;
+    let current_version = format!("v{}", env!("CARGO_PKG_VERSION"));
+
+    if current_version != latest_release {
+        let result = MessageDialog::new()
+            .set_title("Update available")
+            .set_description(format!("A new version of TinyWiiBackupManager is available: {}.\nDo you want to download it?", latest_release))
+            .set_buttons(rfd::MessageButtons::YesNo)
+            .show();
+
+        if result == MessageDialogResult::Yes {
+            open::that(RELEASES_BASE_URL.to_owned() + &latest_release)?;
+        }
+    }
+
+    Ok(())
+}
