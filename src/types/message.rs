fb2394a73b4d06f9a2a0b34d894c06767f836bee
Manuel Quarneti <manuelquarneti@protonmail.com>
2023-11-11 15:29:10 +0100

    :construction: wip

---
+++ src/types/message.rs
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2023 Manuel Quarneti <manuel.quarneti@proton.me>
+// SPDX-License-Identifier: GPL-2.0-only
+
+use crate::types::drive::Drive;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    SelectDrive(Drive),
+    OpenDrive,
+    SelectGame(usize, bool),
+    AddGames(Drive),
+    AddingGames((Drive, Vec<PathBuf>)),
+    RemoveGames,
+}
