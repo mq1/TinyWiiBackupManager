7314a835a8ea1a324cf00f141e8afd954f7d23c8
Manuel Quarneti <manuelquarneti@protonmail.com>
2023-12-04 10:55:36 +0100

    :sparkles: Add games

--- src/pages/games.rs
+++ src/pages/games.rs
@@ -4,10 +4,34 @@
 use eframe::egui::{self, FontId, RichText};
 use egui_modal::{Icon, Modal};
 use poll_promise::Promise;
+use rfd::FileDialog;
+use std::thread;
 
 use crate::app::App;
 
 pub fn view(ctx: &egui::Context, app: &mut App) {
+    if let Some((i, total)) = *app.adding_games_progress.lock().unwrap() {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::Area::new("adding_games_progress")
+                .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::ZERO)
+                .show(ctx, |ui| {
+                    ui.heading("Adding games");
+
+                    ui.add_space(10.0);
+                    ui.spinner();
+                    ui.add_space(10.0);
+
+                    ui.label(&format!("{}/{}", i, total));
+                });
+        });
+
+        if app.games.is_some() {
+            app.games = None;
+        }
+
+        return;
+    }
+
     let drive = app.current_drive.clone().unwrap();
 
     let drive_cloned = drive.clone();
@@ -61,12 +85,6 @@ pub fn view(ctx: &egui::Context, app: &mut App) {
                 });
 
                 ui.horizontal(|ui| {
-                    if ui.button("🗑 Delete selected").clicked() {
-                        delete_games_dialog.open();
-                    }
-
-                    if ui.button("➕ Add games").clicked() {}
-
                     if ui.button("✅ Select all").clicked() {
                         for game in games.iter_mut() {
                             game.checked = true;
@@ -77,6 +95,33 @@ pub fn view(ctx: &egui::Context, app: &mut App) {
                         for game in games.iter_mut() {
                             game.checked = false;
                         }
+                    }
+
+                    if ui.button("➕ Add games").clicked() {
+                        let files = FileDialog::new()
+                            .add_filter("WII Game", &["iso", "wbfs"])
+                            .pick_files();
+
+                        if let Some(files) = files {
+                            let adding_games_progress = app.adding_games_progress.clone();
+                            let drive_cloned = drive.clone();
+
+                            thread::spawn(move || {
+                                *adding_games_progress.lock().unwrap() =
+                                    Some((0usize, files.len()));
+
+                                for (i, file) in files.iter().enumerate() {
+                                    drive_cloned.add_game(file).unwrap();
+                                    adding_games_progress.lock().unwrap().unwrap().0 = i + 1;
+                                }
+
+                                *adding_games_progress.lock().unwrap() = None;
+                            });
+                        }
+                    }
+
+                    if ui.button("🗑 Delete selected").clicked() {
+                        delete_games_dialog.open();
                     }
                 });
 
