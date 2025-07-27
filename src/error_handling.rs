4a1f0bf1c905aa1f4d23a10602cd5e9b52c238c2
Manuel Quarneti <mq1@ik.me>
2025-07-27 13:55:19 +0200

    refactoring

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 use rfd::{MessageDialog, MessageLevel};
+use anyhow::Error;
 
 /// Show an error dialog with the given title and description
 pub fn show_error(title: &str, description: &str) {
@@ -10,4 +11,13 @@ pub fn show_error(title: &str, description: &str) {
         .set_title(title)
         .set_description(description)
         .show();
+}
+
+/// Show an anyhow::Error in an error dialog
+pub fn show_anyhow_error(title: &str, error: &Error) {
+    let mut description = format!("Error: {error}");
+    for cause in error.chain().skip(1) {
+        description.push_str(&format!("\nCaused by: {cause}"));
+    }
+    show_error(title, &description);
 }
