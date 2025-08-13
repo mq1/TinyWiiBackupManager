baaf580c859b7b75ef0c47b92620761880db09da
Manuel Quarneti <mq1@ik.me>
2025-08-13 22:28:00 +0200

    ✨ Windows icon

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
+#[cfg(windows)]
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("logo.ico");
-        res.compile().unwrap();
-    }
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("logo.ico");
+    res.compile().unwrap();
 }
+
+#[cfg(unix)]
+fn main() {}
