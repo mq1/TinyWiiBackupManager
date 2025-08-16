ffb271c79ca36f1c2d2ed798d79a84c88b881d23
Manuel Quarneti <mq1@ik.me>
2025-08-16 11:00:32 +0200

    ci: Fix linux ci

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -51,7 +51,12 @@ echo "Running linuxdeploy..."
     --appdir "${PRODUCT_NAME}.AppDir" \
     --executable "target/release/${APP_NAME}" \
     --desktop-file "${INPUT_DIR}/${APP_NAME}.desktop" \
-    --icon-file "${ASSETS_DIR}/linux/*/${APP_NAME}.png" \
+    --icon-file "${ASSETS_DIR}/linux/32x32/${APP_NAME}.png" \
+    --icon-file "${ASSETS_DIR}/linux/48x48/${APP_NAME}.png" \
+    --icon-file "${ASSETS_DIR}/linux/64x64/${APP_NAME}.png" \
+    --icon-file "${ASSETS_DIR}/linux/128x128/${APP_NAME}.png" \
+    --icon-file "${ASSETS_DIR}/linux/256x256/${APP_NAME}.png" \
+    --icon-file "${ASSETS_DIR}/linux/512x512/${APP_NAME}.png" \
     --output appimage
 
 # 6. Rename the final artifact
