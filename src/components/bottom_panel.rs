9d515805a12eec77328c29869f35cdf47391b8a3
Manuel Quarneti <mq1@ik.me>
2025-08-18 20:08:06 +0200

    feat: Remove sources button

--- src/components/bottom_panel.rs
+++ src/components/bottom_panel.rs
@@ -5,12 +5,17 @@ use crate::app::App;
 use eframe::egui;
 
 /// Renders the bottom panel
-pub fn ui_bottom_panel(ctx: &egui::Context, app: &App) {
-    if let Some(update_info) = &app.version_check_result {
-        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+pub fn ui_bottom_panel(ctx: &egui::Context, app: &mut App) {
+    egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+        if let Some(update_info) = &app.version_check_result {
             let update_text = format!("⚠ Update available: {}", update_info.version);
             ui.hyperlink_to(update_text, &update_info.url)
                 .on_hover_text("Update to the latest version");
+        }
+
+        ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+            ui.checkbox(&mut app.remove_sources, "💣 Remove sources")
+                .on_hover_text("⚠ Remove input files after conversion ⚠");
         });
-    }
+    });
 }
