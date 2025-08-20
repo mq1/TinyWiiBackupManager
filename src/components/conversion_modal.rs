16ab0f2249da3f30c693e066c864d9f7bca38840
Luke Street <luke@street.dev>
2025-08-19 21:00:14 -0600

    Improve conversion progress

--- src/components/conversion_modal.rs
+++ src/components/conversion_modal.rs
@@ -3,6 +3,7 @@
 
 use crate::app::{App, ConversionState};
 use eframe::egui;
+use size::Size;
 
 pub fn ui_conversion_modal(ctx: &egui::Context, app: &App) {
     egui::Modal::new(egui::Id::new("conversion_modal")).show(ctx, |ui| {
@@ -17,14 +18,23 @@ pub fn ui_conversion_modal(ctx: &egui::Context, app: &App) {
             if let ConversionState::Converting {
                 total_files,
                 files_converted,
+                current_progress: (current_progress, total),
             } = app.conversion_state
             {
-                let progress = files_converted as f32 / total_files as f32;
+                let mut progress = files_converted as f32 / total_files as f32;
+                if total > 0 {
+                    progress += (current_progress as f32 / total as f32) / total_files as f32;
+                }
 
                 ui.add(egui::ProgressBar::new(progress).show_percentage());
                 ui.add_space(5.0);
 
                 ui.label(format!("File {} of {}", files_converted + 1, total_files));
+                ui.label(format!(
+                    "Progress: {} / {}",
+                    Size::from_bytes(current_progress),
+                    Size::from_bytes(total)
+                ));
             }
         });
     });
