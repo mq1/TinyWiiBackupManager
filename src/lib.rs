6ebe6413561973dba131b4460ed0eb769e451d8e
Manuel Quarneti <mq1@ik.me>
2025-08-21 20:10:26 +0200

    style: UI improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2025 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: GPL-2.0-only
 
+pub const PRODUCT_NAME: &str = "TinyWiiBackupManager";
+
 mod app;
 mod components;
 pub mod error_handling;
