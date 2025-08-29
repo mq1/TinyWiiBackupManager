5235671b311865c1e6b72b53db64f9b588a41ecc
Manuel Quarneti <mq1@ik.me>
2025-08-29 11:46:44 +0200

    :sparkles: Map CRCs

--- src/components/game_info.rs
+++ src/components/game_info.rs
@@ -31,7 +31,7 @@ fn ui_game_info_content(
 
     ui.horizontal(|ui| {
         ui.label(RichText::new("🆔 Game ID:").strong());
-        ui.label(RichText::new(game.id.iter().collect::<String>()).monospace());
+        ui.label(RichText::new(&game.id_str).monospace());
     });
 
     ui.horizontal(|ui| {
