4bef7b4923325ab24d571144dc9f28459d3b31e3
Manuel Quarneti <mq1@ik.me>
2025-08-31 00:18:29 +0200

    feat: Better error reporting

--- src/messages.rs
+++ src/messages.rs
@@ -7,12 +7,13 @@ use crate::update_check::UpdateInfo;
 use anyhow::Error;
 use eframe::egui;
 use log::{error, info};
+use std::sync::Arc;
 
 /// Messages that can be sent from background tasks to the main thread
 #[derive(Debug, Clone)]
 pub enum BackgroundMessage {
     /// Signal that an error occurred
-    Error(String),
+    Error(Arc<Error>),
     /// Informational message
     Info(String),
     /// Signal that the base directory has changed
@@ -28,7 +29,7 @@ pub enum BackgroundMessage {
 /// Implement the From trait to automatically convert anyhow::Error into our message.
 impl From<Error> for BackgroundMessage {
     fn from(e: Error) -> Self {
-        BackgroundMessage::Error(format!("{e}"))
+        BackgroundMessage::Error(Arc::new(e))
     }
 }
 
@@ -45,13 +46,14 @@ pub fn handle_messages(app: &mut App, ctx: &egui::Context) {
             }
 
             BackgroundMessage::Error(e) => {
-                error!("{e}");
-                components::toasts::show_error_toast(app, &e);
+                error!("{e:?}");
+                let msg = format!("{e:#}");
+                components::toasts::show_error_toast(app, &msg);
             }
 
-            BackgroundMessage::Info(e) => {
-                info!("{e}");
-                components::toasts::show_info_toast(app, &e);
+            BackgroundMessage::Info(msg) => {
+                info!("{msg}");
+                components::toasts::show_info_toast(app, &msg);
             }
 
             BackgroundMessage::GotUpdate(update) => {
