cbe8af94e5e71353e3bb21f89b5ec7f942b18c96
Manuel Quarneti <mq1@ik.me>
2025-09-04 13:51:27 +0200

    :zap: Improve disc meta handling

--- src/gui/game_info.rs
+++ src/gui/game_info.rs
@@ -78,41 +78,37 @@ fn ui_game_info_content(
 
     ui.heading("💿 Disc Metadata");
     ui.add_space(5.0);
-    if let Some(meta) = &game.disc_meta {
-        ui.horizontal(|ui| {
-            ui.label(RichText::new("💿 Format:").strong());
-            ui.label(meta.format.to_string());
-        });
+    ui.horizontal(|ui| {
+        ui.label(RichText::new("💿 Format:").strong());
+        ui.label(game.disc_meta.format.to_string());
+    });
 
-        ui.horizontal(|ui| {
-            ui.label(RichText::new("📦 Compression:").strong());
-            ui.label(meta.compression.to_string());
-        });
+    ui.horizontal(|ui| {
+        ui.label(RichText::new("📦 Compression:").strong());
+        ui.label(game.disc_meta.compression.to_string());
+    });
 
-        if let Some(block_size) = meta.block_size {
-            ui.horizontal(|ui| {
-                ui.label(RichText::new("🧱 Block size:").strong());
-                ui.label(Size::from_bytes(block_size).to_string());
-            });
+    if let Some(block_size) = game.disc_meta.block_size {
+        ui.horizontal(|ui| {
+            ui.label(RichText::new("🧱 Block size:").strong());
+            ui.label(Size::from_bytes(block_size).to_string());
+        });
+    }
+
+    ui.horizontal(|ui| {
+        ui.label(RichText::new("✅ Lossless:").strong());
+        if game.disc_meta.lossless {
+            ui.colored_label(egui::Color32::DARK_GREEN, "Yes");
+        } else {
+            ui.colored_label(egui::Color32::DARK_RED, "No");
         }
+    });
 
+    if let Some(disc_size) = game.disc_meta.disc_size {
         ui.horizontal(|ui| {
-            ui.label(RichText::new("✅ Lossless:").strong());
-            if meta.lossless {
-                ui.colored_label(egui::Color32::DARK_GREEN, "Yes");
-            } else {
-                ui.colored_label(egui::Color32::DARK_RED, "No");
-            }
+            ui.label(RichText::new("💾 Original size:").strong());
+            ui.label(Size::from_bytes(disc_size).to_string());
         });
-
-        if let Some(disc_size) = meta.disc_size {
-            ui.horizontal(|ui| {
-                ui.label(RichText::new("💾 Original size:").strong());
-                ui.label(Size::from_bytes(disc_size).to_string());
-            });
-        }
-    } else {
-        ui.label(RichText::new("Couldn't read disc metadata").color(ui.visuals().warn_fg_color));
     }
 
     ui.add_space(10.0);
@@ -122,66 +118,62 @@ fn ui_game_info_content(
     ui.heading("🔍 Integrity Metadata");
     ui.add_space(5.0);
 
-    if let Some(meta) = &game.disc_meta {
-        // if all are None, show a message
-        if meta.crc32.is_none() && meta.md5.is_none() && meta.sha1.is_none() && meta.xxh64.is_none()
-        {
+    // if all are None, show a message
+    if game.disc_meta.crc32.is_none()
+        && game.disc_meta.md5.is_none()
+        && game.disc_meta.sha1.is_none()
+        && game.disc_meta.xxh64.is_none()
+    {
+        ui.label(RichText::new("No integrity info available").color(ui.visuals().warn_fg_color));
+    }
+
+    if let Some(crc32) = game.disc_meta.crc32 {
+        ui.horizontal(|ui| {
             ui.label(
-                RichText::new("No integrity info available").color(ui.visuals().warn_fg_color),
+                RichText::new("CRC32:")
+                    .text_style(egui::TextStyle::Monospace)
+                    .strong(),
             );
-        }
+            ui.label(
+                RichText::new(format!("{:08x}", crc32)).text_style(egui::TextStyle::Monospace),
+            );
 
-        if let Some(crc32) = meta.crc32 {
-            ui.horizontal(|ui| {
-                ui.label(
-                    RichText::new("CRC32:")
-                        .text_style(egui::TextStyle::Monospace)
-                        .strong(),
-                );
-                ui.label(
-                    RichText::new(format!("{:08x}", crc32)).text_style(egui::TextStyle::Monospace),
-                );
+            ui_game_checks(ui, &game);
+        });
+    }
 
-                ui_game_checks(ui, &game);
-            });
-        }
+    if let Some(md5) = game.disc_meta.md5 {
+        ui.horizontal(|ui| {
+            ui.label(
+                RichText::new("MD5:")
+                    .text_style(egui::TextStyle::Monospace)
+                    .strong(),
+            );
+            ui.label(RichText::new(hex::encode(md5)).text_style(egui::TextStyle::Monospace));
+        });
+    }
 
-        if let Some(md5) = meta.md5 {
-            ui.horizontal(|ui| {
-                ui.label(
-                    RichText::new("MD5:")
-                        .text_style(egui::TextStyle::Monospace)
-                        .strong(),
-                );
-                ui.label(RichText::new(hex::encode(md5)).text_style(egui::TextStyle::Monospace));
-            });
-        }
+    if let Some(sha1) = game.disc_meta.sha1 {
+        ui.horizontal(|ui| {
+            ui.label(
+                RichText::new("SHA-1:")
+                    .text_style(egui::TextStyle::Monospace)
+                    .strong(),
+            );
+            ui.label(RichText::new(hex::encode(sha1)).text_style(egui::TextStyle::Monospace));
+        });
+    }
 
-        if let Some(sha1) = meta.sha1 {
-            ui.horizontal(|ui| {
-                ui.label(
-                    RichText::new("SHA-1:")
-                        .text_style(egui::TextStyle::Monospace)
-                        .strong(),
-                );
-                ui.label(RichText::new(hex::encode(sha1)).text_style(egui::TextStyle::Monospace));
-            });
-        }
-
-        if let Some(xxhash64) = meta.xxh64 {
-            ui.horizontal(|ui| {
-                ui.label(
-                    RichText::new("XXH64:")
-                        .text_style(egui::TextStyle::Monospace)
-                        .strong(),
-                );
-                ui.label(
-                    RichText::new(format!("{:016x}", xxhash64))
-                        .text_style(egui::TextStyle::Monospace),
-                );
-            });
-        }
-    } else {
-        ui.label(RichText::new("Couldn't read disc metadata").color(ui.visuals().warn_fg_color));
+    if let Some(xxhash64) = game.disc_meta.xxh64 {
+        ui.horizontal(|ui| {
+            ui.label(
+                RichText::new("XXH64:")
+                    .text_style(egui::TextStyle::Monospace)
+                    .strong(),
+            );
+            ui.label(
+                RichText::new(format!("{:016x}", xxhash64)).text_style(egui::TextStyle::Monospace),
+            );
+        });
     }
 }
