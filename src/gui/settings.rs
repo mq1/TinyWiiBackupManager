0b8da834e1600fc4dab50671995c945d57f033a9
Manuel Quarneti <mq1@ik.me>
2025-09-05 11:54:02 +0200

    style: Add StripPartitions UI

--- src/gui/settings.rs
+++ src/gui/settings.rs
@@ -2,13 +2,14 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 use crate::app::App;
-use crate::settings::WiiOutputFormat;
+use crate::settings::{StripPartitions, WiiOutputFormat};
 use eframe::egui;
 use egui_theme_switch::global_theme_switch;
 use strum::IntoEnumIterator;
 
 pub fn ui_settings_window(ctx: &egui::Context, app: &mut App) {
     egui::Window::new("⚙ Settings")
+        .default_width(600.0)
         .open(&mut app.settings_window_open)
         .show(ctx, |ui| {
             ui.add_space(10.0);
@@ -18,6 +19,17 @@ pub fn ui_settings_window(ctx: &egui::Context, app: &mut App) {
 
             for format in WiiOutputFormat::iter() {
                 ui.radio_value(&mut app.settings.wii_output_format, format, format.as_ref());
+            }
+
+            ui.add_space(10.0);
+            ui.separator();
+            ui.add_space(10.0);
+
+            ui.heading("🗑 Strip Partitions to save space");
+            ui.add_space(10.0);
+
+            for format in StripPartitions::iter() {
+                ui.radio_value(&mut app.settings.strip_partitions, format, format.as_ref());
             }
 
             ui.add_space(10.0);
