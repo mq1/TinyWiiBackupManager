dc7f99f866a542064f33f9da6d9e6fe2672fdcb9
Manuel Quarneti <mq1@ik.me>
2025-09-07 23:24:18 +0200

    :recycle: Refactoring

--- src/util/wiiload.rs
+++ src/util/wiiload.rs
@@ -13,14 +13,11 @@ use std::time::Duration;
 use zip::write::SimpleFileOptions;
 use zip::{ZipArchive, ZipWriter};
 
-const WIILOAD_MAGIC: &[u8] = b"HAXX";
-const WIILOAD_MAJOR: &[u8] = &[0];
-const WIILOAD_MINOR: &[u8] = &[5];
 const WIILOAD_PORT: u16 = 4299;
+const WIILOAD_MAGIC: &[u8] = &[b'H', b'A', b'X', b'X', 0, 5, 0, 8]; // 8 is app.zip\0 len
 const WIILOAD_ARGS: &[u8] = b"app.zip\0";
-const WIILOAD_ARGS_LEN: &[u8] = &[0, 8];
 const WIILOAD_TIMEOUT: Duration = Duration::from_secs(10);
-const WIILOAD_CHUNK_SIZE: usize = 128 * 1024;
+const WIILOAD_CHUNK_SIZE: usize = 4 * 1024;
 
 pub fn push(source_zip: impl AsRef<Path>, wii_ip: &str) -> Result<()> {
     let source_zip_name = source_zip
@@ -138,9 +135,6 @@ fn send_to_wii(
 
     // Send Wiiload header
     stream.write_all(WIILOAD_MAGIC)?;
-    stream.write_all(WIILOAD_MAJOR)?;
-    stream.write_all(WIILOAD_MINOR)?;
-    stream.write_all(WIILOAD_ARGS_LEN)?;
     stream.write_all(&compressed_len.to_be_bytes())?;
     stream.write_all(&uncompressed_len.to_be_bytes())?;
 
