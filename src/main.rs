88873a744888f7ef41b680e2dbd6b6f6e7a950b8
Manuel Quarneti <mq1@ik.me>
2025-09-23 22:47:34 +0200

    :rotating_light: fix compilation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #![warn(clippy::all, rust_2018_idioms)]
-#![allow(non_snake_case)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use TinyWiiBackupManager::app::App;
