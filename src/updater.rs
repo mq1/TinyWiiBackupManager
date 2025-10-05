94a1c75acfaafe0c35fd3913ef2d9b668889212d
Manuel Quarneti <mq1@ik.me>
2025-10-05 19:14:50 +0200

    ♻️ Refactoring

--- src/updater.rs
+++ src/updater.rs
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: 2025 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use crate::{UpdateInfo, http::AGENT, tasks};
-use anyhow::{Context, Result};
+use crate::{UpdateInfo, http::AGENT, show_err, tasks};
+use anyhow::Context;
 use slint::ToSharedString;
 
 const VERSION_URL: &str = concat!(
@@ -10,44 +10,37 @@ const VERSION_URL: &str = concat!(
     "/releases/latest/download/version.txt"
 );
 
-/// Checks for a newer version of the application.
-fn check() -> Result<UpdateInfo> {
-    let latest_version = AGENT
-        .get(VERSION_URL)
-        .call()
-        .context("Failed to fetch version")?
-        .body_mut()
-        .read_to_string()
-        .context("Failed to decode response body as UTF-8")?;
+pub fn check() {
+    if let Err(e) = tasks::spawn(Box::new(|weak| {
+        let latest_version = AGENT
+            .get(VERSION_URL)
+            .call()
+            .context("Failed to fetch version")?
+            .body_mut()
+            .read_to_string()
+            .context("Failed to decode response body as UTF-8")?;
 
-    let latest_version = latest_version.trim();
+        let latest_version = latest_version.trim();
 
-    let info = if latest_version != env!("CARGO_PKG_VERSION") {
-        UpdateInfo {
-            version: latest_version.to_shared_string(),
-            url: format!(
-                "{}/releases/tag/{}",
-                env!("CARGO_PKG_REPOSITORY"),
-                latest_version
-            )
-            .to_shared_string(),
+        if latest_version != env!("CARGO_PKG_VERSION") {
+            let info = UpdateInfo {
+                version: latest_version.to_shared_string(),
+                url: format!(
+                    "{}/releases/tag/{}",
+                    env!("CARGO_PKG_REPOSITORY"),
+                    latest_version
+                )
+                .to_shared_string(),
+            };
+
+            weak.upgrade_in_event_loop(|handle| {
+                handle.set_update_info(info);
+                handle.invoke_show_update();
+            })?;
         }
-    } else {
-        UpdateInfo::default()
-    };
-
-    Ok(info)
-}
-
-pub fn spawn_task() {
-    tasks::spawn(Box::new(|weak| {
-        let info = check().context("Failed to check for updates")?;
-
-        weak.upgrade_in_event_loop(|handle| {
-            handle.set_update_info(info);
-            handle.invoke_show_update();
-        })?;
 
         Ok(())
-    }));
+    })) {
+        show_err(e);
+    }
 }
