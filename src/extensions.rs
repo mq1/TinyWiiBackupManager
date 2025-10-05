71637670e601b539b70355d3f09dd65306c4d678
Manuel Quarneti <mq1@ik.me>
2025-10-06 00:21:12 +0200

    refactor: Extension handling

--- src/extensions.rs
+++ src/extensions.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use crate::ArchiveFormat;
+use crate::{ArchiveFormat, WiiOutputFormat, config};
 
 pub const SUPPORTED_INPUT_EXTENSIONS: &'static [&'static str] = &[
     "gcm", "iso", "wbfs", "wia", "rvz", "ciso", "gcz", "tgc", "nfs",
@@ -13,5 +13,22 @@ impl ArchiveFormat {
             ArchiveFormat::Rvz => "rvz",
             ArchiveFormat::Iso => "iso",
         }
+    }
+}
+
+impl WiiOutputFormat {
+    pub fn extension(&self) -> &'static str {
+        match self {
+            WiiOutputFormat::WbfsAuto | WiiOutputFormat::WbfsFixed => "wbfs",
+            WiiOutputFormat::Iso => "iso",
+        }
+    }
+}
+
+pub fn get_convert_extension(is_wii: bool) -> &'static str {
+    if is_wii {
+        config::get().wii_output_format.extension()
+    } else {
+        "iso"
     }
 }
