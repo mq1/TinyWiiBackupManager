f5db04a28f640b53f0689a039933f833914afc0b
Manuel Quarneti <mq1@ik.me>
2025-10-17 02:17:53 +0200

    💄 Increase font weight

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
+use slint_build::EmbedResourcesKind;
 use std::{collections::HashMap, env::var, path::Path};
 
 fn main() {
@@ -13,7 +14,9 @@ fn main() {
 
     let config = slint_build::CompilerConfiguration::new()
         .with_library_paths(library_paths)
-        .with_style("material".into());
+        .with_style("material".into())
+        .embed_resources(EmbedResourcesKind::EmbedFiles);
+
     slint_build::compile_with_config("ui/main.slint", config).unwrap();
 
     // Windows-specific icon resource
