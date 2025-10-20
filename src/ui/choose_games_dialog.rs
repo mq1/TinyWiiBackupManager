3468291dc85ae90bfe4baabbce14cee45a241fbc
Manuel Quarneti <mq1@ik.me>
2025-10-20 19:51:22 +0200

    ✨ Automatically download covers

--- src/ui/choose_games_dialog.rs
+++ src/ui/choose_games_dialog.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use crate::{app::App, convert};
+use crate::{app::App, convert, covers, games};
 use eframe::egui;
 
 pub fn update(ctx: &egui::Context, app: &mut App) {
@@ -9,5 +9,7 @@ pub fn update(ctx: &egui::Context, app: &mut App) {
 
     if let Some(paths) = app.choose_games.take_picked_multiple() {
         convert::spawn_add_games_task(&app.config, paths, &app.task_processor);
+        games::spawn_get_games_task(app);
+        covers::spawn_download_covers_task(app);
     }
 }
