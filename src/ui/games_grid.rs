b2e406b12e7042977bd06c37804dfb5ee0b651ce
Manuel Quarneti <mq1@ik.me>
2025-10-21 17:56:49 +0200

    💄 Update GC icon

--- src/ui/games_grid.rs
+++ src/ui/games_grid.rs
@@ -59,7 +59,7 @@ fn view_game_card(
             // Top row with console label on the left and size label on the right
             ui.horizontal(|ui| {
                 // Console label on the left
-                ui.label(if game.is_wii { "🎾 Wii" } else { "◼ GC" });
+                ui.label(if game.is_wii { "🎾 Wii" } else { "🎲 GC" });
 
                 // Size label on the right
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
