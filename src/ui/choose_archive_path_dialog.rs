19ad9ba021e3435c1f73cf1ebd93f98a5ceade19
Manuel Quarneti <mq1@ik.me>
2025-10-25 03:00:44 +0200

    ⚗️ test

--- src/ui/choose_archive_path_dialog.rs
+++ src/ui/choose_archive_path_dialog.rs
@@ -5,10 +5,11 @@ use crate::{app::App, archive};
 use eframe::egui;
 
 pub fn update(ctx: &egui::Context, app: &mut App) {
-    if app.choose_archive_path.show(ctx).selected()
-        && let Some(out_path) = app.choose_games.path()
+    app.choose_archive_path.update(ctx);
+
+    if let Some(out_path) = app.choose_games.take_picked()
         && let Some(game_dir) = app.archiving_game.take()
     {
-        archive::spawn_archive_game_task(app, game_dir, out_path.to_path_buf());
+        archive::spawn_archive_game_task(app, game_dir, out_path);
     }
 }
--- src/ui/choose_games_dialog.rs
+++ src/ui/choose_games_dialog.rs
@@ -3,17 +3,11 @@
 
 use crate::{app::App, convert};
 use eframe::egui;
-use std::path::PathBuf;
 
 pub fn update(ctx: &egui::Context, app: &mut App) {
-    if app.choose_games.show(ctx).selected() && !app.choose_games.selection().is_empty() {
-        convert::spawn_add_games_task(
-            app,
-            app.choose_games
-                .selection()
-                .iter()
-                .map(PathBuf::from)
-                .collect(),
-        );
+    app.choose_games.update(ctx);
+
+    if let Some(paths) = app.choose_games.take_picked_multiple() {
+        convert::spawn_add_games_task(app, paths);
     }
 }
