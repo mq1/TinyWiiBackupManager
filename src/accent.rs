c4440447e25cb221f2373b2a4865414ad9ab5ede
Manuel Quarneti <mq1@ik.me>
2025-10-27 21:38:01 +0100

    💄 More accurate colors

--- src/accent.rs
+++ src/accent.rs
@@ -13,14 +13,14 @@ pub fn system_accent_color() -> Option<egui::Color32> {
         .ok()?;
 
     match String::from_utf8_lossy(&output.stdout).trim() {
-        "0" => Some(egui::Color32::RED.gamma_multiply_u8(127)),
-        "1" => Some(egui::Color32::ORANGE.gamma_multiply_u8(127)),
-        "2" => Some(egui::Color32::YELLOW.gamma_multiply_u8(127)),
-        "3" => Some(egui::Color32::GREEN.gamma_multiply_u8(127)),
-        "4" => Some(egui::Color32::BLUE.gamma_multiply_u8(127)),
-        "5" => Some(egui::Color32::PURPLE.gamma_multiply_u8(127)),
-        "6" => Some(egui::Color32::MAGENTA.gamma_multiply_u8(127)),
-        "-1" => Some(egui::Color32::GRAY.gamma_multiply_u8(127)),
+        "0" => Some(egui::Color32::from_rgba_unmultiplied(236, 95, 93, 127)), // red
+        "1" => Some(egui::Color32::from_rgba_unmultiplied(232, 136, 58, 127)), // orange
+        "2" => Some(egui::Color32::from_rgba_unmultiplied(246, 200, 68, 127)), // yellow
+        "3" => Some(egui::Color32::from_rgba_unmultiplied(120, 184, 86, 127)), // green
+        "4" => Some(egui::Color32::from_rgba_unmultiplied(52, 120, 246, 127)), // blue
+        "5" => Some(egui::Color32::from_rgba_unmultiplied(154, 85, 163, 127)), // purple
+        "6" => Some(egui::Color32::from_rgba_unmultiplied(228, 92, 156, 127)), // pink
+        "-1" => Some(egui::Color32::from_rgba_unmultiplied(140, 140, 140, 127)), // gray
         _ => None,
     }
 }
@@ -33,6 +33,7 @@ pub fn system_accent_color() -> Option<String> {
     let settings = UISettings::new().ok()?;
     let color = settings.GetColorValue(UIColorType::Accent).ok()?;
 
-    let color = egui::Color32::from_rgba_unmultiplied(color.R, color.G, color.B, color.A);
-    Some(color)
+    Some(egui::Color32::from_rgba_unmultiplied(
+        color.R, color.G, color.B, 127,
+    ))
 }
