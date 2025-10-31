e30ce911cf5f85ecf2a14edc27a6cb32b953ebff
Manuel Quarneti <mq1@ik.me>
2025-10-31 14:13:08 +0100

    🚸 Update label [skip ci]

--- src/ui/tools.rs
+++ src/ui/tools.rs
@@ -29,7 +29,7 @@ pub fn update(ctx: &egui::Context, app: &mut App) {
                     covers::spawn_download_all_covers_task(app);
                 }
 
-                ui.label("Download all covers (will default to English on PAL games, while usbloader_gx detects the system language)");
+                ui.label("Download all covers (defaults to English for PAL games, while usbloader_gx downloads them in the correct language)");
             });
 
             ui.horizontal(|ui| {
