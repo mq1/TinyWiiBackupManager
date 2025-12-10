81057fde9cada91226928b5dd4a409737c212f6a
Manuel Quarneti <mq1@ik.me>
2025-12-10 19:37:09 +0100

    perf: Improve overflow reader performance

--- src/checksum.rs
+++ src/checksum.rs
@@ -92,7 +92,7 @@ pub fn calc_crc32(game_dir: &Path, msg_sender: &Sender<Message>) -> Result<u32> 
 
     let disc = if let Some(overflow) = overflow {
         let reader = OverflowReader::new(&path, &overflow)?;
-        DiscReader::new_from_non_cloneable_read(reader, &get_disc_opts())?
+        DiscReader::new_stream(Box::new(reader), &get_disc_opts())?
     } else {
         DiscReader::new(&path, &get_disc_opts())?
     };
