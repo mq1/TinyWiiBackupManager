c593111276cc6cb4c2d120d29ed266f0b5dea0ed
Manuel Quarneti <mq1@ik.me>
2025-12-30 12:32:52 +0100

    chore: add update-winget.sh

---
+++ scripts/update-winget.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+komac update mq1.TinyWiiBackupManager \
+  --version "${1}" \
+  --urls \
+  "https://github.com/mq1/TinyWiiBackupManager/releases/download/v${1}/TinyWiiBackupManager-v${1}-windows-x86_64-setup.exe" \
+  "https://github.com/mq1/TinyWiiBackupManager/releases/download/v${1}/TinyWiiBackupManager-v${1}-windows-arm64-setup.exe" \
+  "https://github.com/mq1/TinyWiiBackupManager/releases/download/v${1}/TinyWiiBackupManager-v${1}-windows-x86-setup.exe" \
+  --submit
