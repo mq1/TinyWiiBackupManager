f89803d78fb80332167083e570e6333cb5f75e1c
Manuel Quarneti <mq1@ik.me>
2026-01-06 01:02:09 +0100

    refactor: remove duplicate code

--- src/http_util.rs
+++ src/http_util.rs
@@ -1,13 +1,11 @@
 // SPDX-FileCopyrightText: 2026 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use anyhow::{Result, anyhow, bail};
-use async_zip::base::read::mem::ZipFileReader;
-use smol::{
-    fs::{self, File},
-    io::{self, BufWriter},
-};
+use anyhow::Result;
+use smol::fs::{self};
 use std::path::Path;
+
+use crate::util;
 
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
@@ -38,34 +36,5 @@ pub async fn download_and_extract_zip(uri: &str, dest_dir: &Path) -> Result<()> 
         .send()?
         .into_bytes();
 
-    let zip = ZipFileReader::new(body).await?;
-    for (i, entry) in zip.file().entries().iter().enumerate() {
-        let filename = entry.filename().as_str()?;
-        let is_dir = entry.dir()?;
-
-        let rel_path = Path::new(filename);
-        let dest_path = dest_dir.join(rel_path).canonicalize()?;
-
-        if !dest_path.starts_with(dest_dir) {
-            bail!("Directory traversal attempt detected");
-        }
-
-        if is_dir {
-            fs::create_dir_all(&dest_path).await?;
-            continue;
-        }
-
-        let parent = dest_path
-            .parent()
-            .ok_or(anyhow!("Failed to get parent dir"))?;
-
-        let mut reader = zip.reader_without_entry(i).await?;
-
-        fs::create_dir_all(parent).await?;
-        let file = File::create(dest_path).await?;
-        let mut writer = BufWriter::new(file);
-        io::copy(&mut reader, &mut writer).await?;
-    }
-
-    Ok(())
+    util::extract_zip_bytes(body, dest_dir).await
 }
