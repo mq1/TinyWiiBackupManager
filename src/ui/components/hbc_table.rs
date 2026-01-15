20348236dbdc47d0fcb053732945ef9a9eb28ed8
Manuel Quarneti <mq1@ik.me>
2026-01-15 12:28:31 +0100

    perf: enable fat-lto for zstd

--- src/ui/components/hbc_table.rs
+++ src/ui/components/hbc_table.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use crate::{
-    hbc::HbcApp,
     message::Message,
     state::State,
     ui::{Screen, style},
@@ -15,16 +14,16 @@ use lucide_icons::iced::{icon_info, icon_trash};
 
 pub fn view(state: &State) -> Element<'_, Message> {
     let t_columns = vec![
-        table::column(text("Name").size(16), |(_, app): (usize, &HbcApp)| {
-            text(&app.meta.name)
+        table::column(text("Name").size(16), |i: usize| {
+            text(&state.hbc_apps[i].meta.name)
         }),
-        table::column(text("Version").size(16), |(_, app): (usize, &HbcApp)| {
-            text(&app.meta.version)
+        table::column(text("Version").size(16), |i: usize| {
+            text(&state.hbc_apps[i].meta.version)
         }),
-        table::column(text("Size").size(16), |(_, app): (usize, &HbcApp)| {
-            text(app.size.to_string())
+        table::column(text("Size").size(16), |i: usize| {
+            text(state.hbc_apps[i].size.to_string())
         }),
-        table::column(text("Actions").size(16), |(i, _): (usize, &HbcApp)| {
+        table::column(text("Actions").size(16), |i| {
             row![
                 button(row![icon_info(), text("Info")].spacing(5))
                     .style(style::rounded_secondary_button)
@@ -38,15 +37,11 @@ pub fn view(state: &State) -> Element<'_, Message> {
     ];
 
     let table = if !state.hbc_filter.is_empty() {
-        let apps = state
-            .filtered_hbc_indices
-            .iter()
-            .copied()
-            .map(|i| (i, &state.hbc_apps[i]));
-        table(t_columns, apps).width(Length::Fill)
+        let indices = state.filtered_hbc_indices.iter().copied();
+        table(t_columns, indices).width(Length::Fill)
     } else {
-        let apps = state.hbc_apps.iter().enumerate();
-        table(t_columns, apps).width(Length::Fill)
+        let indices = 0..state.hbc_apps.len();
+        table(t_columns, indices).width(Length::Fill)
     };
 
     container(table).padding(10).into()
