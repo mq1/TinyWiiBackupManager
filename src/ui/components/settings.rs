82182936eca2e408ce5a979bde9db9c34fdc4c81
Manuel Quarneti <mq1@ik.me>
2026-01-20 20:42:56 +0100

    styling

--- src/ui/components/settings.rs
+++ src/ui/components/settings.rs
@@ -21,226 +21,230 @@ pub fn view(state: &State) -> Element<'_, Message> {
     let remove_sources_apps = state.config.remove_sources_apps();
     let txt_codes_source = state.config.txt_codes_source();
 
-    scrollable(
-        column![
-            row![icon_settings().size(18), text("Settings").size(18)].spacing(5),
-            container(
-                column![
-                    row![icon_disc_3(), text("Wii output format")].spacing(5),
-                    rule::horizontal(1),
-                    space(),
-                    radio(
-                        "WBFS (recommended)",
-                        nod::common::Format::Wbfs,
-                        Some(wii_output_format),
-                        |format| Message::UpdateConfig(
-                            state.config.clone_with_wii_output_format(format)
-                        )
-                    ),
-                    radio(
-                        "ISO (very large)",
-                        nod::common::Format::Iso,
-                        Some(wii_output_format),
-                        |format| Message::UpdateConfig(
-                            state.config.clone_with_wii_output_format(format)
-                        )
-                    ),
-                ]
-                .spacing(5)
-                .padding(10)
-                .width(Length::Fill)
-            )
-            .style(style::card),
-            container(
-                column![
-                    row![icon_disc_3(), text("GameCube output format")].spacing(5),
-                    rule::horizontal(1),
-                    space(),
-                    radio(
-                        "ISO (recommended)",
-                        nod::common::Format::Iso,
-                        Some(gc_output_format),
-                        |format| Message::UpdateConfig(
-                            state.config.clone_with_gc_output_format(format)
-                        )
-                    ),
-                    radio(
-                        "CISO (much smaller, slightly slower, less metadata in game loaders)",
-                        nod::common::Format::Ciso,
-                        Some(gc_output_format),
-                        |format| Message::UpdateConfig(
-                            state.config.clone_with_gc_output_format(format)
-                        )
-                    ),
-                ]
-                .spacing(5)
-                .padding(10)
-                .width(Length::Fill)
-            )
-            .style(style::card),
-            container(
-                column![
-                    row![icon_file_archive(), text("Archive format")].spacing(5),
-                    rule::horizontal(1),
-                    space(),
-                    radio(
-                        "RVZ zstd-19 (recommended)",
-                        nod::common::Format::Rvz,
-                        Some(archive_format),
-                        |format| Message::UpdateConfig(
-                            state.config.clone_with_archive_format(format)
-                        )
-                    ),
-                    radio(
-                        "ISO (very large)",
-                        nod::common::Format::Iso,
-                        Some(archive_format),
-                        |format| Message::UpdateConfig(
-                            state.config.clone_with_archive_format(format)
-                        )
-                    ),
-                ]
-                .spacing(5)
-                .padding(10)
-                .width(Length::Fill)
-            )
-            .style(style::card),
-            container(
-                column![
-                    row![icon_square_split_horizontal(), text("Split Output")].spacing(5),
-                    rule::horizontal(1),
-                    space(),
-                    radio(
-                        "Only when needed (recommended)",
-                        false,
-                        Some(always_split),
-                        |yes| Message::UpdateConfig(state.config.clone_with_always_split(yes))
-                    ),
-                    radio("Always 4GB-32KB", true, Some(always_split), |yes| {
-                        Message::UpdateConfig(state.config.clone_with_always_split(yes))
-                    }),
-                ]
-                .spacing(5)
-                .padding(10)
-                .width(Length::Fill)
-            )
-            .style(style::card),
-            container(
-                column![
-                    row![
-                        icon_file_minus_corner(),
-                        text("Remove Update Partition on WBFS/CISO")
+    column![
+        row![icon_settings().size(18), text("Settings").size(18)]
+            .spacing(5)
+            .padding(10),
+        scrollable(
+            column![
+                container(
+                    column![
+                        row![icon_disc_3(), text("Wii output format")].spacing(5),
+                        rule::horizontal(1),
+                        space(),
+                        radio(
+                            "WBFS (recommended)",
+                            nod::common::Format::Wbfs,
+                            Some(wii_output_format),
+                            |format| Message::UpdateConfig(
+                                state.config.clone_with_wii_output_format(format)
+                            )
+                        ),
+                        radio(
+                            "ISO (very large)",
+                            nod::common::Format::Iso,
+                            Some(wii_output_format),
+                            |format| Message::UpdateConfig(
+                                state.config.clone_with_wii_output_format(format)
+                            )
+                        ),
                     ]
-                    .spacing(5),
-                    rule::horizontal(1),
-                    space(),
-                    radio(
-                        "No (recommended)",
-                        false,
-                        Some(scrub_update_partition),
-                        |yes| Message::UpdateConfig(
-                            state.config.clone_with_scrub_update_partition(yes)
-                        )
-                    ),
-                    radio(
-                        "Yes (saves some space; update partition is zeroed, but still there)",
-                        true,
-                        Some(scrub_update_partition),
-                        |yes| Message::UpdateConfig(
-                            state.config.clone_with_scrub_update_partition(yes)
-                        )
-                    ),
-                ]
-                .spacing(5)
-                .padding(10)
-                .width(Length::Fill)
-            )
-            .style(style::card),
-            container(
-                column![
-                    row![icon_trash(), text("Delete sources when adding games")].spacing(5),
-                    rule::horizontal(1),
-                    space(),
-                    radio(
-                        "No (recommended)",
-                        false,
-                        Some(remove_sources_games),
-                        |yes| Message::UpdateConfig(
-                            state.config.clone_with_remove_sources_games(yes)
-                        )
-                    ),
-                    radio("Yes", true, Some(remove_sources_games), |yes| {
-                        Message::UpdateConfig(state.config.clone_with_remove_sources_games(yes))
-                    }),
-                ]
-                .spacing(5)
-                .padding(10)
-                .width(Length::Fill)
-            )
-            .style(style::card),
-            container(
-                column![
-                    row![icon_trash(), text("Delete sources when adding apps")].spacing(5),
-                    rule::horizontal(1),
-                    space(),
-                    radio(
-                        "No (recommended)",
-                        false,
-                        Some(remove_sources_apps),
-                        |yes| Message::UpdateConfig(
-                            state.config.clone_with_remove_sources_apps(yes)
-                        )
-                    ),
-                    radio("Yes", true, Some(remove_sources_apps), |yes| {
-                        Message::UpdateConfig(state.config.clone_with_remove_sources_apps(yes))
-                    }),
-                ]
-                .spacing(5)
-                .padding(10)
-                .width(Length::Fill)
-            )
-            .style(style::card),
-            container(
-                column![
-                    row![icon_skull(), text("Cheat Codes source")].spacing(5),
-                    rule::horizontal(1),
-                    space(),
-                    radio(
-                        "geckocodes.org (web.archive.org)    (Recommended, high quality)",
-                        TxtCodesSource::WebArchive,
-                        Some(txt_codes_source),
-                        |source| Message::UpdateConfig(
-                            state.config.clone_with_txt_codes_source(source)
-                        )
-                    ),
-                    radio(
-                        "codes.rc24.xyz    (Lower quality)",
-                        TxtCodesSource::Rc24,
-                        Some(txt_codes_source),
-                        |source| Message::UpdateConfig(
-                            state.config.clone_with_txt_codes_source(source)
-                        )
-                    ),
-                    radio(
-                        "gamehacking.org    (Up to date, download might fail)",
-                        TxtCodesSource::GameHacking,
-                        Some(txt_codes_source),
-                        |source| Message::UpdateConfig(
-                            state.config.clone_with_txt_codes_source(source)
-                        )
-                    ),
-                ]
-                .spacing(5)
-                .padding(10)
-                .width(Length::Fill)
-            )
-            .style(style::card)
-        ]
-        .spacing(10)
-        .padding(10)
-        .width(Length::Fill),
-    )
-    .spacing(2)
-    .height(Length::Fill)
+                    .spacing(5)
+                    .padding(10)
+                    .width(Length::Fill)
+                )
+                .style(style::card),
+                container(
+                    column![
+                        row![icon_disc_3(), text("GameCube output format")].spacing(5),
+                        rule::horizontal(1),
+                        space(),
+                        radio(
+                            "ISO (recommended)",
+                            nod::common::Format::Iso,
+                            Some(gc_output_format),
+                            |format| Message::UpdateConfig(
+                                state.config.clone_with_gc_output_format(format)
+                            )
+                        ),
+                        radio(
+                            "CISO (much smaller, slightly slower, less metadata in game loaders)",
+                            nod::common::Format::Ciso,
+                            Some(gc_output_format),
+                            |format| Message::UpdateConfig(
+                                state.config.clone_with_gc_output_format(format)
+                            )
+                        ),
+                    ]
+                    .spacing(5)
+                    .padding(10)
+                    .width(Length::Fill)
+                )
+                .style(style::card),
+                container(
+                    column![
+                        row![icon_file_archive(), text("Archive format")].spacing(5),
+                        rule::horizontal(1),
+                        space(),
+                        radio(
+                            "RVZ zstd-19 (recommended)",
+                            nod::common::Format::Rvz,
+                            Some(archive_format),
+                            |format| Message::UpdateConfig(
+                                state.config.clone_with_archive_format(format)
+                            )
+                        ),
+                        radio(
+                            "ISO (very large)",
+                            nod::common::Format::Iso,
+                            Some(archive_format),
+                            |format| Message::UpdateConfig(
+                                state.config.clone_with_archive_format(format)
+                            )
+                        ),
+                    ]
+                    .spacing(5)
+                    .padding(10)
+                    .width(Length::Fill)
+                )
+                .style(style::card),
+                container(
+                    column![
+                        row![icon_square_split_horizontal(), text("Split output")].spacing(5),
+                        rule::horizontal(1),
+                        space(),
+                        radio(
+                            "Only when needed (recommended)",
+                            false,
+                            Some(always_split),
+                            |yes| Message::UpdateConfig(state.config.clone_with_always_split(yes))
+                        ),
+                        radio("Always 4GB-32KB", true, Some(always_split), |yes| {
+                            Message::UpdateConfig(state.config.clone_with_always_split(yes))
+                        }),
+                    ]
+                    .spacing(5)
+                    .padding(10)
+                    .width(Length::Fill)
+                )
+                .style(style::card),
+                container(
+                    column![
+                        row![
+                            icon_file_minus_corner(),
+                            text("Remove update partition on WBFS/CISO")
+                        ]
+                        .spacing(5),
+                        rule::horizontal(1),
+                        space(),
+                        radio(
+                            "No (recommended)",
+                            false,
+                            Some(scrub_update_partition),
+                            |yes| Message::UpdateConfig(
+                                state.config.clone_with_scrub_update_partition(yes)
+                            )
+                        ),
+                        radio(
+                            "Yes (saves some space; update partition is zeroed, but still there)",
+                            true,
+                            Some(scrub_update_partition),
+                            |yes| Message::UpdateConfig(
+                                state.config.clone_with_scrub_update_partition(yes)
+                            )
+                        ),
+                    ]
+                    .spacing(5)
+                    .padding(10)
+                    .width(Length::Fill)
+                )
+                .style(style::card),
+                container(
+                    column![
+                        row![icon_trash(), text("Delete sources when adding games")].spacing(5),
+                        rule::horizontal(1),
+                        space(),
+                        radio(
+                            "No (recommended)",
+                            false,
+                            Some(remove_sources_games),
+                            |yes| Message::UpdateConfig(
+                                state.config.clone_with_remove_sources_games(yes)
+                            )
+                        ),
+                        radio("Yes", true, Some(remove_sources_games), |yes| {
+                            Message::UpdateConfig(state.config.clone_with_remove_sources_games(yes))
+                        }),
+                    ]
+                    .spacing(5)
+                    .padding(10)
+                    .width(Length::Fill)
+                )
+                .style(style::card),
+                container(
+                    column![
+                        row![icon_trash(), text("Delete sources when adding apps")].spacing(5),
+                        rule::horizontal(1),
+                        space(),
+                        radio(
+                            "No (recommended)",
+                            false,
+                            Some(remove_sources_apps),
+                            |yes| Message::UpdateConfig(
+                                state.config.clone_with_remove_sources_apps(yes)
+                            )
+                        ),
+                        radio("Yes", true, Some(remove_sources_apps), |yes| {
+                            Message::UpdateConfig(state.config.clone_with_remove_sources_apps(yes))
+                        }),
+                    ]
+                    .spacing(5)
+                    .padding(10)
+                    .width(Length::Fill)
+                )
+                .style(style::card),
+                container(
+                    column![
+                        row![icon_skull(), text("Cheat codes source")].spacing(5),
+                        rule::horizontal(1),
+                        space(),
+                        radio(
+                            "geckocodes.org (web.archive.org)    (Recommended, high quality)",
+                            TxtCodesSource::WebArchive,
+                            Some(txt_codes_source),
+                            |source| Message::UpdateConfig(
+                                state.config.clone_with_txt_codes_source(source)
+                            )
+                        ),
+                        radio(
+                            "codes.rc24.xyz    (Lower quality)",
+                            TxtCodesSource::Rc24,
+                            Some(txt_codes_source),
+                            |source| Message::UpdateConfig(
+                                state.config.clone_with_txt_codes_source(source)
+                            )
+                        ),
+                        radio(
+                            "gamehacking.org    (Up to date, download might fail)",
+                            TxtCodesSource::GameHacking,
+                            Some(txt_codes_source),
+                            |source| Message::UpdateConfig(
+                                state.config.clone_with_txt_codes_source(source)
+                            )
+                        ),
+                    ]
+                    .spacing(5)
+                    .padding(10)
+                    .width(Length::Fill)
+                )
+                .style(style::card)
+            ]
+            .spacing(10)
+            .padding(10)
+            .width(Length::Fill)
+            .height(Length::Fill)
+        )
+        .spacing(1)
+    ]
     .into()
 }
