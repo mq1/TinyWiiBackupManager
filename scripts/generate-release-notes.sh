95abe14ba11dc0af1acb28bcd0cb4447d18879b4
Manuel Quarneti <mq1@ik.me>
2026-01-27 17:56:12 +0100

    doc: update release notes generation

--- scripts/generate-release-notes.sh
+++ scripts/generate-release-notes.sh
@@ -6,6 +6,9 @@ fi
 
 awk -v t="$1" '/^## \[/ && f {exit} $0 ~ "^## \\[" t {f=1; next} f' CHANGELOG.md
 
-echo "&rarr; [Windows Installer] (https://github.com/mq1/TinyWiiBackupManagerInstaller/releases/latest/download/TinyWiiBackupManagerInstaller.exe)\\"
-echo "&rarr; [macOS universal binary](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-macos-universal.dmg)\\"
-echo "&rarr; [Linux Flatpak](https://flathub.org/apps/details/it.mq1.TinyWiiBackupManager)"
+echo "## ⬇️ Recommended downloads for the majority of users\n"
+echo "→ [Windows Installer](https://github.com/mq1/TinyWiiBackupManagerInstaller/releases/latest/download/TinyWiiBackupManagerInstaller.exe)\\"
+echo "→ [Windws x64 Standalone](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-windows-x86_64.zip)\\"
+echo "→ [macOS Universal Binary](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-macos-universal.dmg)\\"
+echo "→ [Linux Flatpak](https://flathub.org/apps/details/it.mq1.TinyWiiBackupManager)\\"
+echo "→ [Linux x86_64 AppImage](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-linux-x86_64.AppImage)"
