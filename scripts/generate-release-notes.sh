ae84bf6c9bd7814b160f28f5e4a1132869a2c4fa
Manuel Quarneti <mq1@ik.me>
2026-01-27 18:15:58 +0100

    typo

--- scripts/generate-release-notes.sh
+++ scripts/generate-release-notes.sh
@@ -8,7 +8,7 @@ awk -v t="$1" '/^## \[/ && f {exit} $0 ~ "^## \\[" t {f=1; next} f' CHANGELOG.md
 
 echo "## ⬇️ Recommended downloads for the majority of users\n"
 echo "→ [Windows Installer](https://github.com/mq1/TinyWiiBackupManagerInstaller/releases/latest/download/TinyWiiBackupManagerInstaller.exe)\\"
-echo "→ [Windws x64 Standalone](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-windows-x86_64.zip)\\"
+echo "→ [Windows x64 Standalone](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-windows-x86_64.zip)\\"
 echo "→ [macOS Universal Binary](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-macos-universal.dmg)\\"
 echo "→ [Linux Flatpak](https://flathub.org/apps/details/it.mq1.TinyWiiBackupManager)\\"
 echo "→ [Linux x86_64 AppImage](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-linux-x86_64.AppImage)"
