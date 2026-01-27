6af82ae7232e33de0273f5907da825f4bce6d02b
Manuel Quarneti <mq1@ik.me>
2026-01-27 14:11:08 +0100

    ci: generate release notes from CHANGELOG.md

---
+++ scripts/generate-release-notes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+awk -v t="$1" '/^## \[/ && f {exit} $0 ~ "^## \\[" t {f=1; next} f' CHANGELOG.md
+
+echo "&rarr; [Windows Installer] (https://github.com/mq1/TinyWiiBackupManagerInstaller/releases/latest/download/TinyWiiBackupManagerInstaller.exe)\\"
+echo "&rarr; [macOS universal binary](https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-macos-universal.dmg)\\"
+echo "&rarr; [Linux Flatpak](https://flathub.org/apps/details/it.mq1.TinyWiiBackupManager)"
