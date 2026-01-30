dd1a7c77842f0e793ee6d8e9f8aee0ee9a5eac57
Manuel Quarneti <mq1@ik.me>
2026-01-30 16:23:02 +0100

    perf: optimize id_map

--- src/games/id_map.rs
+++ src/games/id_map.rs
@@ -12,10 +12,6 @@ const COMPRESSED_ID_MAP: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/id_ma
 
 include!(concat!(env!("OUT_DIR"), "/id_map_meta.rs"));
 
-fn u24_le_to_u32(buf: [u8; 3]) -> u32 {
-    u32::from_le_bytes([buf[0], buf[1], buf[2], 0])
-}
-
 pub struct IdMap {
     data: Box<[u8]>,
     ids: [GameID; ID_COUNT],
@@ -57,36 +53,47 @@ fn deserialize_id_map() -> IdMap {
 
     let data = unsafe { buf.assume_init() };
 
+    // Prepare containers
     let mut ids = MaybeUninit::<[GameID; ID_COUNT]>::uninit();
     let mut ghids = Box::<[u32]>::new_uninit_slice(ID_COUNT);
     let mut title_offsets = Box::<[usize]>::new_uninit_slice(ID_COUNT);
 
-    let mut cursor = 0;
+    // Get the raw pointer to the start of the data
+    let base_ptr = data.as_ptr();
+    let mut offset = 0;
     let mut i = 0;
 
+    // We can hoist the ids pointer calculation out of the loop
+    let ids_ptr = ids.as_mut_ptr().cast::<GameID>();
+
     while i < ID_COUNT {
-        // Parse game id (6 bytes)
-        let gid_slice = &data[cursor..cursor + 6];
-        let game_id: [u8; 6] = gid_slice.try_into().unwrap();
         unsafe {
-            let ids_ptr = ids.as_mut_ptr().cast::<GameID>();
-            ids_ptr.add(i).write(GameID::from(game_id));
+            // Calculate pointer to current record
+            let curr_ptr = base_ptr.add(offset);
+
+            // Parse Game ID (6 bytes)
+            // Cast u8 pointer directly to [u8; 6] pointer and dereference
+            let gid_bytes = *curr_ptr.cast::<[u8; 6]>();
+            ids_ptr.add(i).write(GameID::from(gid_bytes));
+
+            // Parse GameHacking ID (3 bytes)
+            // Offset 6 bytes from current record start
+            let gh_bytes = *curr_ptr.add(6).cast::<[u8; 3]>();
+            let gh_val = u32::from_le_bytes([gh_bytes[0], gh_bytes[1], gh_bytes[2], 0]);
+            ghids[i].write(gh_val);
+
+            // Parse Title Length (1 byte)
+            // Offset 9 bytes (6 + 3)
+            let str_len = *curr_ptr.add(9) as usize;
+
+            // Store the Title Offset
+            // The string bytes start 10 bytes in (6 + 3 + 1)
+            title_offsets[i].write(offset + 10);
+
+            // Advance offset: 10 bytes of headers + length of string
+            offset += 10 + str_len;
+            i += 1;
         }
-        cursor += 6;
-
-        // Parse gamehacking id (3 bytes)
-        let gh_slice = &data[cursor..cursor + 3];
-        let gamehacking_id = u24_le_to_u32(gh_slice.try_into().unwrap());
-        ghids[i].write(gamehacking_id);
-        cursor += 3;
-
-        // Parse title
-        let str_len = data[cursor] as usize;
-        cursor += 1;
-        title_offsets[i].write(cursor);
-
-        cursor += str_len;
-        i += 1;
     }
 
     let ids = unsafe { ids.assume_init() };
