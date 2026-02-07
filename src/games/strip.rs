825a56d8f076af9880e5f2a8dfebdd086b629e35
Manuel Quarneti <mq1@ik.me>
2026-02-07 22:27:09 +0100

    fix: potential fix for #525

--- src/games/strip.rs
+++ src/games/strip.rs
@@ -65,78 +65,83 @@ impl StripOperation {
                 let must_split = self.always_split || self.is_fat32;
 
                 let out_path = disc_info.disc_path().with_extension("wbfs.new");
-                let out_file = File::create(&out_path)?;
-                let mut out_writer = BufWriter::new(out_file);
                 let mut overflow_path: Option<PathBuf> = None;
-                let mut overflow_writer: Option<BufWriter<File>> = None;
 
-                let disc_opts = DiscOptions {
-                    partition_encryption: PartitionEncryption::Original,
-                    preloader_threads,
-                };
-                let disc_reader = DiscReader::new(disc_info.disc_path(), &disc_opts)?;
+                {
+                    let out_file = File::create(&out_path)?;
+                    let mut out_writer = BufWriter::new(out_file);
+                    let mut overflow_writer: Option<BufWriter<File>> = None;
 
-                let out_opts = format_to_opts(Format::Wbfs);
-                let disc_writer = DiscWriter::new(disc_reader, &out_opts)?;
+                    let disc_opts = DiscOptions {
+                        partition_encryption: PartitionEncryption::Original,
+                        preloader_threads,
+                    };
+                    let disc_reader = DiscReader::new(disc_info.disc_path(), &disc_opts)?;
 
-                let mut prev_percentage = 100;
-                let mut bytes_written_in_first_split = 0;
-                let game_title = self.source.title();
-                let finalization = disc_writer.process(
+                    let out_opts = format_to_opts(Format::Wbfs);
+                    let disc_writer = DiscWriter::new(disc_reader, &out_opts)?;
+
+                    let mut prev_percentage = 100;
+                    let mut bytes_written_in_first_split = 0;
+                    let game_title = self.source.title();
+                    let finalization = disc_writer.process(
                     |data, progress, total| {
-                        if let Some(overflow_writer) = &mut overflow_writer {
-                            overflow_writer.write_all(&data)?;
-                        } else if must_split {
-                            let remaining_in_first_split =
-                                SPLIT_SIZE - bytes_written_in_first_split;
+                            if let Some(overflow_writer) = &mut overflow_writer {
+                                overflow_writer.write_all(&data)?;
+                            } else if must_split {
+                                let remaining_in_first_split =
+                                    SPLIT_SIZE - bytes_written_in_first_split;
 
-                            let bytes_to_write_count = data.len();
+                                let bytes_to_write_count = data.len();
 
-                            if remaining_in_first_split < bytes_to_write_count {
-                                let overflow_path = overflow_path.insert(disc_info.disc_path().with_extension("wbf1.new"));
-                                let overflow_file = File::create(overflow_path)?;
-                                let overflow_writer =
-                                    overflow_writer.insert(BufWriter::new(overflow_file));
+                                if remaining_in_first_split < bytes_to_write_count {
+                                    let overflow_path = overflow_path.insert(disc_info.disc_path().with_extension("wbf1.new"));
+                                    let overflow_file = File::create(overflow_path)?;
+                                    let overflow_writer =
+                                        overflow_writer.insert(BufWriter::new(overflow_file));
 
-                                #[allow(clippy::cast_possible_truncation)]
-                                out_writer.write_all(&data[..remaining_in_first_split])?;
-                                overflow_writer.write_all(&data[remaining_in_first_split..])?;
+                                    #[allow(clippy::cast_possible_truncation)]
+                                    out_writer.write_all(&data[..remaining_in_first_split])?;
+                                    overflow_writer.write_all(&data[remaining_in_first_split..])?;
 
-                                // we don't update bytes_written_in_first_split as we won't access
-                                // it anymore; theoretically it should now be SPLIT_SIZE
+                                    // we don't update bytes_written_in_first_split as we won't access
+                                    // it anymore; theoretically it should now be SPLIT_SIZE
+                                } else {
+                                    out_writer.write_all(&data)?;
+                                    bytes_written_in_first_split += bytes_to_write_count;
+                                }
                             } else {
                                 out_writer.write_all(&data)?;
-                                bytes_written_in_first_split += bytes_to_write_count;
                             }
-                        } else {
-                            out_writer.write_all(&data)?;
-                        }
 
-                        let progress_percentage = progress * 100 / total;
-                        if progress_percentage != prev_percentage {
-                            let _ = tx.try_send(format!(
-                                "Remove update partition from {game_title}  {progress_percentage:02}%"
-                            ));
-                            prev_percentage = progress_percentage;
-                        }
+                            let progress_percentage = progress * 100 / total;
+                            if progress_percentage != prev_percentage {
+                                let _ = tx.try_send(format!(
+                                    "Remove update partition from {game_title}  {progress_percentage:02}%"
+                                ));
+                                prev_percentage = progress_percentage;
+                            }
 
-                        Ok(())
-                    },
+                            Ok(())
+                        },
                     &process_opts,
-                )?;
+                    )?;
 
-                if !finalization.header.is_empty() {
-                    out_writer.rewind()?;
-                    out_writer.write_all(&finalization.header)?;
+                    if !finalization.header.is_empty() {
+                        out_writer.rewind()?;
+                        out_writer.write_all(&finalization.header)?;
+                    }
+
+                    out_writer.flush()?;
+                    if let Some(overflow_writer) = &mut overflow_writer {
+                        overflow_writer.flush()?;
+                    }
                 }
 
-                out_writer.flush()?;
-                if let Some(overflow_writer) = &mut overflow_writer {
-                    overflow_writer.flush()?;
-                }
-
+                let _ = fs::remove_file(disc_info.disc_path());
                 fs::rename(out_path, disc_info.disc_path())?;
                 if let Some(overflow_path) = overflow_path.take() {
+                    let _ = fs::remove_file(disc_info.disc_path().with_extension("wbf1"));
                     fs::rename(overflow_path, disc_info.disc_path().with_extension("wbf1"))?;
                 }
 
