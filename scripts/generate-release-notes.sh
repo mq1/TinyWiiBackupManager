28a7f2d3e9917f6619e1b7bffb3109fd468551aa
Manuel Quarneti <mq1@ik.me>
2026-03-06 20:40:11 +0100

    refactoring

--- scripts/generate-release-notes.sh
+++ scripts/generate-release-notes.sh
@@ -4,7 +4,7 @@ if ! grep -Fq "## [$1]" CHANGELOG.md; then
   exit 1
 fi
 
-awk -v t="$1" '/^## \[/ && f {exit} $0 ~ "^## \\[" t {f=1; next} f' CHANGELOG.md
+sed -n "/^## \[$1\]/,/^## \[/{/^## \[/d;p;}" CHANGELOG.md
 
 cat <<EOF
 <br> 
@@ -15,11 +15,11 @@ cat <<EOF
   </tr>
   <tr>
     <td>
-    :window: <a href="https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-windows-x86_64.zip">Windows x64 Standalone</a>
-    <br>
-    :apple: <a href="https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-macos-universal.dmg">macOS Universal Binary</a>
-    <br>
-    :penguin: <a href="https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-linux-x86_64.AppImage">Linux x86_64 AppImage</a>
+      :window: <a href="https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-windows-x86_64.zip">Windows x64 Standalone</a>
+      <br>
+      :apple: <a href="https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-macos-universal.dmg">macOS Universal Binary</a>
+      <br>
+      :penguin: <a href="https://github.com/mq1/TinyWiiBackupManager/releases/download/$1/TinyWiiBackupManager-$1-linux-x86_64.AppImage">Linux x86_64 AppImage</a>
     </td>
   </tr>
 </table>
