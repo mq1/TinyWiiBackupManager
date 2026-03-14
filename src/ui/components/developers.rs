71a97fae12826d73a02937c80ed34057bc34013c
Manuel Quarneti <mq1@ik.me>
2026-03-14 22:58:06 +0100

    style: add Aep icon to developers

--- src/ui/components/developers.rs
+++ src/ui/components/developers.rs
@@ -2,13 +2,14 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use iced::widget::Text;
-use lucide_icons::iced::{icon_user, icon_wand_sparkles};
+use lucide_icons::iced::{icon_metronome, icon_user, icon_wand_sparkles};
 
 /// Map of developers to their icons
 /// If you want to add yourself, open a PR or an issue :)
 pub fn get_icon(developer: &str) -> Text<'_> {
     match developer {
-        "blackb0x" => icon_wand_sparkles(),
+        "blackb0x" | "USB Loader GX Team" => icon_wand_sparkles(),
+        "Aep" => icon_metronome(),
         _ => icon_user(),
     }
 }
