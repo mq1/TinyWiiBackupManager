dfcb71be84544f9e1078d6339567c406dfadcac8
Manuel Quarneti <mq1@ik.me>
2026-03-15 14:49:18 +0100

    refactoring

--- scripts/package-linux-appimage.sh
+++ scripts/package-linux-appimage.sh
@@ -4,15 +4,18 @@
 
 VERSION_NAME=$1
 ARCH_NAME=$2
-APPIMAGETOOL=$3
-APPIMAGE_ARCH=$4
+export ARCH=$3
+export VERSION="${VERSION_NAME#v}"
+
+# Download appimagetool
+wget -q "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-${ARCH}.AppImage" -O appimagetool
+chmod +x appimagetool
 
 cp -r package/linux/AppDir TinyWiiBackupManager.AppDir
 install -Dm0755 TinyWiiBackupManager TinyWiiBackupManager.AppDir/usr/bin/TinyWiiBackupManager
+
 mkdir -p dist
-VERSION="${VERSION_NAME#v}" \
-  ARCH="$APPIMAGE_ARCH" \
-  "$APPIMAGETOOL" \
+./appimagetool \
   --appimage-extract-and-run \
   -u "gh-releases-zsync|mq1|TinyWiiBackupManager|latest|*$ARCH_NAME.AppImage.zsync" \
   TinyWiiBackupManager.AppDir \
