705c6584e314aadca3dc7656864086d699208164
Manuel Quarneti <mq1@ik.me>
2026-03-15 19:06:03 +0100

    ci: refactoring

--- scripts/package-windows.ps1
+++ scripts/package-windows.ps1
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2026 Manuel Quarneti <mq1@ik.me>
 # SPDX-License-Identifier: GPL-3.0-only
 
+$ErrorActionPreference = "Stop"
+
 param (
     [string]$VersionName,
     [string]$Platform,
