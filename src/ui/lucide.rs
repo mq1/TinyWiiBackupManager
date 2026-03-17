2e10c8418647775737f7055812d833463ca39faa
Manuel Quarneti <mq1@ik.me>
2026-03-17 11:17:00 +0100

    refactoring

--- src/ui/lucide.rs
+++ src/ui/lucide.rs
@@ -8,13 +8,8 @@ include!(concat!(env!("OUT_DIR"), "/lucide_meta.rs"));
 const COMPRESSED_BYTES: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/lucide.ttf.zst"));
 
 pub fn get_load_lucide_task() -> Task<Message> {
-    Task::future(async { load_lucide() })
+    Task::future(async { zstd::bulk::decompress(COMPRESSED_BYTES, LUCIDE_BYTES_LEN).unwrap() })
         .then(font::load)
         .map_err(|e| format!("Failed to load lucide.ttf: {e:#?}"))
         .map(Message::EmptyResult)
-}
-
-fn load_lucide() -> Vec<u8> {
-    zstd::bulk::decompress(COMPRESSED_BYTES, LUCIDE_BYTES_LEN)
-        .expect("Failed to decompress lucide.ttf.zst")
 }
