5ca5506394ae55d5e7f57d72a9155a85f2ddaf62
Manuel Quarneti <mq1@ik.me>
2026-03-24 11:47:12 +0100

    ci: fix

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -3,30 +3,37 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 VERSION_NAME=$1
-ARCH_NAME=$2
-
 export VERSION="${VERSION_NAME#v}"
 
-if [ "$ARCH_NAME" == "x86_64-v3" ]; then
-  export ARCH=x86_64
-elif [ "$ARCH_NAME" == "x86_64-v2" ]; then
-  export ARCH=x86_64
-elif [ "$ARCH_NAME" == "x86_64" ]; then
-  export ARCH=x86_64
-elif [ "$ARCH_NAME" == "x86" ]; then
-  export ARCH=i686
-elif [ "$ARCH_NAME" == "arm64" ]; then
-  export ARCH=aarch64
-elif [ "$ARCH_NAME" == "armhf" ]; then
-  export ARCH=armhf
-fi
+ARCH_NAME=$2
 
+case "$ARCH_NAME" in
+x86_64*)
+  export ARCH=x86_64
+  ;;
+x86)
+  export ARCH=i686
+  ;;
+arm64)
+  export ARCH=aarch64
+  ;;
+armhf)
+  export ARCH=armhf
+  ;;
+esac
+
+# Setup appdir
 cp -r package/linux/AppDir TinyWiiBackupManager.AppDir
 install -Dm0755 TinyWiiBackupManager TinyWiiBackupManager.AppDir/usr/bin/TinyWiiBackupManager
 
+# Download appimagetool
+wget "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage" -O appimagetool
+chmod +x appimagetool
+
 mkdir -p dist
-"/usr/bin/appimagetool-$ARCH.AppImage" \
+cd dist
+
+../appimagetool \
   -u "gh-releases-zsync|mq1|TinyWiiBackupManager|latest|*$ARCH_NAME.AppImage.zsync" \
-  TinyWiiBackupManager.AppDir \
-  "dist/TinyWiiBackupManager-${VERSION_NAME}-linux-$ARCH_NAME.AppImage"
-cp *.zsync dist/
+  ../TinyWiiBackupManager.AppDir \
+  "TinyWiiBackupManager-${VERSION_NAME}-linux-$ARCH_NAME.AppImage"
