be878e817ddf8dd06f1ec6ad5a8ce9270ff5b181
Manuel Quarneti <mq1@ik.me>
2026-03-27 16:04:11 +0100

    fix

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -23,8 +23,8 @@ armhf)
   export CC_armv7_unknown_linux_gnueabihf="clang-21"
   export AR_armv7_unknown_linux_gnueabihf="llvm-ar-21"
   export CFLAGS_unknown_linux_gnueabihf="-O3 -flto"
-  export PKG_CONFIG="arm-linux-gnueabihf-pkg-config"
   export PKG_CONFIG_PATH="/usr/lib/arm-linux-gnueabihf/pkgconfig"
+  export PKG_CONFIG_ALLOW_CROSS=1
   TARGET="armv7-unknown-linux-gnueabihf"
   ;;
 x86)
@@ -33,8 +33,8 @@ x86)
   export CC_i686_unknown_linux_gnu="clang-21"
   export AR_i686_unknown_linux_gnu="llvm-ar-21"
   export CFLAGS_i686_unknown_linux_gnu="-O3 -flto"
-  export PKG_CONFIG="i686-linux-gnu-pkg-config"
   export PKG_CONFIG_PATH="/usr/lib/i386-linux-gnu/pkgconfig"
+  export PKG_CONFIG_ALLOW_CROSS=1
   TARGET="i686-unknown-linux-gnu"
   ;;
 x86_64)
