36c8d5c36dfed8a27ee1157bd2d697072c49bd6a
Manuel Quarneti <mq1@ik.me>
2026-03-29 17:31:23 +0200

    styling

--- build.rs
+++ build.rs
@@ -102,7 +102,7 @@ fn main() {
     let library = HashMap::from([("lucide".to_string(), PathBuf::from(lucide_slint::lib()))]);
     let config = slint_build::CompilerConfiguration::new()
         .with_library_paths(library)
-        .with_style("fluent".into());
+        .with_style("cupertino".into());
 
     slint_build::compile_with_config("ui/app-window.slint", config).unwrap();
 
