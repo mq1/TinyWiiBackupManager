993fdbfb9080f62d845e8ad1f06340ce7bb558a8
Manuel Quarneti <mq1@ik.me>
2026-04-01 18:36:15 +0200

    refactoring

--- src/state.rs
+++ src/state.rs
@@ -7,56 +7,53 @@ use slint::{Global, Model, VecModel};
 impl State<'_> {
     pub fn handle_callbacks(&self) {
         let weak = self.as_weak();
-        self.on_close_notification(move |i| {
-            let state = weak.upgrade().unwrap();
-
-            #[allow(clippy::cast_sign_loss)]
-            state
-                .get_notifications()
-                .as_any()
-                .downcast_ref::<VecModel<Notification>>()
-                .unwrap()
-                .remove(i as usize);
-        });
+        self.on_close_notification(move |i| weak.upgrade().unwrap().close_notification(i));
 
         let weak = self.as_weak();
-        self.on_apply_sorting(move || {
-            let state = weak.upgrade().unwrap();
-            let sort_by = state.get_config().contents.sort_by;
-            let mut games = state.get_game_list().games.iter().collect::<Vec<_>>();
-            game_list::sort(&mut games, &sort_by);
-
-            state
-                .get_game_list()
-                .games
-                .as_any()
-                .downcast_ref::<VecModel<Game>>()
-                .unwrap()
-                .set_vec(games);
-        });
+        self.on_apply_sorting(move || weak.upgrade().unwrap().apply_sorting());
 
         let weak = self.as_weak();
-        self.on_add_to_queue(move || {
-            let state = weak.upgrade().unwrap();
-
-            let queue = state.get_conversion_queue();
-            let queue = queue
-                .as_any()
-                .downcast_ref::<VecModel<QueuedConversion>>()
-                .unwrap();
-
-            let new = state.get_adding_games();
-            let new = new
-                .as_any()
-                .downcast_ref::<VecModel<QueuedConversion>>()
-                .unwrap();
-
-            queue.extend(new.iter());
-            new.clear();
-        });
+        self.on_add_to_queue(move || weak.upgrade().unwrap().add_to_queue());
 
         let weak = self.as_weak();
         self.on_start_conversion(move || weak.upgrade().unwrap().start_conversion());
+    }
+
+    #[allow(clippy::cast_sign_loss)]
+    pub fn close_notification(&self, i: i32) {
+        self.get_notifications()
+            .as_any()
+            .downcast_ref::<VecModel<Notification>>()
+            .unwrap()
+            .remove(i as usize);
+    }
+
+    pub fn apply_sorting(&self) {
+        let games = self.get_game_list().games;
+        let games = games.as_any().downcast_ref::<VecModel<Game>>().unwrap();
+
+        let sort_by = self.get_config().contents.sort_by;
+        let mut sorted = games.iter().collect::<Vec<_>>();
+        game_list::sort(&mut sorted, &sort_by);
+
+        games.set_vec(sorted);
+    }
+
+    pub fn add_to_queue(&self) {
+        let queue = self.get_conversion_queue();
+        let queue = queue
+            .as_any()
+            .downcast_ref::<VecModel<QueuedConversion>>()
+            .unwrap();
+
+        let adding = self.get_adding_games();
+        let adding = adding
+            .as_any()
+            .downcast_ref::<VecModel<QueuedConversion>>()
+            .unwrap();
+
+        queue.extend(adding.iter());
+        adding.clear();
     }
 
     pub fn start_conversion(&self) {
