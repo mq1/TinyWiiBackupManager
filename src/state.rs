5b0381822546da5935b6d862b113d16d5bec0d9d
Manuel Quarneti <mq1@ik.me>
2026-04-06 17:16:08 +0200

    perf: optimize slint runtime for size

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2026 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use crate::{Game, Notification, QueuedConversion, State, checksum, covers};
+use crate::{checksum, covers, Notification, QueuedConversion, State};
 use slint::{Global, Model, ModelRc, VecModel};
 use std::{
     path::{Path, PathBuf},
