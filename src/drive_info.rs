6cfcf92127a08177ab7f3689bcbd21cc3f9253ac
Manuel Quarneti <mq1@ik.me>
2026-04-11 12:27:29 +0200

    styling

--- src/drive_info.rs
+++ src/drive_info.rs
@@ -9,12 +9,23 @@ use which_fs::FsKind;
 impl DriveInfo {
     pub fn from_path(path: impl AsRef<Path>) -> Self {
         let path = path.as_ref();
+
+        if !path.is_dir() {
+            return Self::default();
+        }
+
+        let label = match path.file_name() {
+            Some(name) => name.to_string_lossy().to_shared_string(),
+            None => path.to_string_lossy().to_shared_string(),
+        };
+
         let (used_gib, total_gib) = get_usage(path);
         let fs_kind = FsKind::try_from_path(path)
             .unwrap_or(FsKind::Unknown)
             .to_shared_string();
 
         Self {
+            label,
             used_gib,
             total_gib,
             fs_kind,
