c0f4fb94cd156dc01215ab066b3c8d847a3c7301
Manuel Quarneti <mq1@ik.me>
2026-05-14 15:42:36 +0200

    Update update.rs

--- crates/gui/src/update.rs
+++ crates/gui/src/update.rs
@@ -209,9 +209,9 @@ impl State {
                 let compare_homebrew_apps = twbm_core::homebrew_app::get_compare_fn(&self.config);
                 self.homebrew_apps.sort_by(compare_homebrew_apps);
 
-                message_queue.push_back((Message::SyncConfig, SharedString::new()));
                 message_queue.push_back((Message::RefreshDisplayedGames, SharedString::new()));
                 message_queue.push_back((Message::RefreshDisplayedHomebrewApps, SharedString::new()));
+                message_queue.push_back((Message::SyncConfig, SharedString::new()));
             }
             Message::SetPreferredLanguage => {
                 let value = args.next().unwrap().parse().unwrap();
