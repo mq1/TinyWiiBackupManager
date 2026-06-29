47e02de94f25ad6bab1d071db82ae1f9a972e2e3
Manuel Quarneti <mq1@ik.me>
2026-06-29 10:38:31 +0200

    refactoring

--- src/notifications.rs
+++ src/notifications.rs
@@ -1,9 +1,7 @@
 // SPDX-FileCopyrightText: 2026 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use derive_getters::Getters;
 use serde::Deserialize;
-use std::fmt;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize)]
 #[serde(rename_all = "snake_case")]
@@ -14,22 +12,11 @@ pub enum NotificationLevel {
     Success,
 }
 
-impl fmt::Display for NotificationLevel {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            NotificationLevel::Info => write!(f, "INFO"),
-            NotificationLevel::Warning => write!(f, "WARNING"),
-            NotificationLevel::Error => write!(f, "ERROR"),
-            NotificationLevel::Success => write!(f, "SUCCESS"),
-        }
-    }
-}
-
-#[derive(Debug, Clone, Deserialize, Getters)]
+#[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub struct Notification {
-    label: String,
-    level: NotificationLevel,
+    pub label: String,
+    pub level: NotificationLevel,
 }
 
 impl Notification {
