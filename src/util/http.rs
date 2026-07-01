9e5016d264c42748e3cdc1b455aa04bc750f5dc3
Manuel Quarneti <mq1@ik.me>
2026-07-01 08:42:34 +0200

    optimize

--- src/util/http.rs
+++ src/util/http.rs
@@ -2,19 +2,22 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use anyhow::Result;
-use async_compat::CompatExt;
-use std::{fs, path::Path};
+use async_compat::Compat;
+use smol::fs;
+use std::path::Path;
 
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
 pub async fn download_file(uri: &str, dest: impl AsRef<Path>) -> Result<()> {
-    let resp = bitreq::get(uri)
-        .with_header("User-Agent", USER_AGENT)
-        .send_async()
-        .compat()
-        .await?;
+    let resp = Compat::new(async {
+        bitreq::get(uri)
+            .with_header("User-Agent", USER_AGENT)
+            .send_async()
+            .await
+    })
+    .await?;
 
-    fs::write(dest, resp.as_bytes())?;
+    fs::write(dest, resp.as_bytes()).await?;
 
     Ok(())
 }
