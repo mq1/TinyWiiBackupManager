9cf112d33058df4204e580f4ac16fde6d73c01fc
Manuel Quarneti <mq1@ik.me>
2026-07-01 08:50:26 +0200

    refactor

--- src/util/http.rs
+++ src/util/http.rs
@@ -9,12 +9,11 @@ use std::path::Path;
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
 pub async fn download_file(uri: &str, dest: impl AsRef<Path>) -> Result<()> {
-    let resp = Compat::new(async {
+    let resp = Compat::new(
         bitreq::get(uri)
             .with_header("User-Agent", USER_AGENT)
-            .send_async()
-            .await
-    })
+            .send_async(),
+    )
     .await?;
 
     fs::write(dest, resp.as_bytes()).await?;
