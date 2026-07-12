6abfa78d81add738901eecd2306fc7c9f4e825df
Manuel Quarneti <mq1@ik.me>
2026-07-13 00:01:19 +0200

    wip

--- src/ui/components/link.rs
+++ src/ui/components/link.rs
@@ -4,12 +4,25 @@
 use crate::messages::Message;
 use iced::{
     Element, Length,
-    advanced::Widget,lumn, container, row, text},
+    widget::{button, column, container, row, text},
 };
 use std::{borrow::Cow, ffi::OsStr};
 
 pub fn view<'a>(label: impl Into<Cow<'a, str>>, url: &'a OsStr) -> Element<'a, Message> {
-    rich_text![span(label.into()).link(url)]
-        .on_link_click(Message::Open)
+    let label = text(label.into());
+    let underline = container(row![].height(1).width(Length::Fill)).style(|theme| {
+        let mut base = container::bordered_box(theme);
+        base.border.color = theme.palette().primary;
+        base
+    });
+
+    button(column![label, underline].width(Length::Shrink))
+        .style(|theme, status| {
+            let mut base = button::text(theme, status);
+            base.text_color = theme.palette().primary;
+            base
+        })
+        .padding(0)
+        .on_press_with(|| Message::Open(url.to_os_string()))
         .into()
 }
