eb87977c77b0b07b46c5d36aa6fd3091e7d41949
Manuel Quarneti <mq1@ik.me>
2026-07-15 10:04:13 +0200

    use isahc

--- src/util/http.rs
+++ src/util/http.rs
@@ -2,21 +2,22 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use anyhow::Result;
-use async_compat::Compat;
+use isahc::{AsyncReadResponseExt, HttpClient};
 use smol::fs;
-use std::path::Path;
+use std::{path::Path, sync::LazyLock};
 
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
-pub async fn download_file(uri: &str, dest: impl AsRef<Path>) -> Result<()> {
-    let resp = Compat::new(
-        bitreq::get(uri)
-            .with_header("User-Agent", USER_AGENT)
-            .send_async(),
-    )
-    .await?;
+static CLIENT: LazyLock<HttpClient> = LazyLock::new(|| {
+    HttpClient::builder()
+        .default_header("User-Agent", USER_AGENT)
+        .build()
+        .unwrap()
+});
 
-    fs::write(dest, resp.as_bytes()).await?;
+pub async fn download_file(uri: &str, dest: impl AsRef<Path>) -> Result<()> {
+    let body = CLIENT.get_async(uri).await?.bytes().await?;
+    fs::write(dest, body).await?;
 
     Ok(())
 }
