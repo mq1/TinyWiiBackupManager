286ac03770f5f4208e7b0d0533433a8eeea132b9
Manuel Quarneti <mq1@ik.me>
2026-09-24 18:47:10 +0200

    fix: some apps not being loaded

--- src/homebrew/meta.rs
+++ src/homebrew/meta.rs
@@ -6,9 +6,7 @@ use std::{fs, path::Path};
 
 fn get_value(contents: &str, start_pattern: &str, end_pattern: &str) -> Option<Box<str>> {
     let start = contents.find(start_pattern)?;
-    let slice = &contents[start..];
-    let start = slice.find('>')? + 1;
-    let slice = &slice[start..];
+    let slice = &contents[start + start_pattern.len() + 1..];
     let end = slice.find(end_pattern)?;
     let value = &slice[..end];
 
@@ -19,17 +17,23 @@ macro_rules! get_property {
     ($contents:expr, $element:literal) => {{
         const START_PATTERN: &str = concat!("<", $element);
         const END_PATTERN: &str = concat!("</", $element, ">");
-        get_value($contents, START_PATTERN, END_PATTERN).context("failed to parse meta.xml")
+        const ERROR: &str = concat!("failed to parse meta.xml: ", $element, " not found");
+
+        get_value($contents, START_PATTERN, END_PATTERN).context(ERROR)
     }};
 }
 
 fn parse_release_date(raw: Box<str>) -> Box<str> {
     if raw.len() >= 8 {
-        let year = &raw[0..4];
-        let month = &raw[4..6];
-        let day = &raw[6..8];
+        let mut s = String::with_capacity(10);
 
-        format!("{year}-{month}-{day}").into_boxed_str()
+        s.push_str(&raw[0..4]);
+        s.push('-');
+        s.push_str(&raw[4..6]);
+        s.push('-');
+        s.push_str(&raw[6..8]);
+
+        s.into_boxed_str()
     } else {
         raw
     }
@@ -54,10 +58,11 @@ impl HomebrewAppMeta {
 
         let name = get_property!(&root, "name")?;
         let version = get_property!(&root, "version")?;
-        let release_date = get_property!(&root, "release_date")?;
-        let coder = get_property!(&root, "coder")?;
-        let short_description = get_property!(&root, "short_description")?;
-        let long_description = get_property!(&root, "long_description")?;
+
+        let release_date = get_property!(&root, "release_date").unwrap_or_default();
+        let coder = get_property!(&root, "coder").unwrap_or_default();
+        let short_description = get_property!(&root, "short_description").unwrap_or_default();
+        let long_description = get_property!(&root, "long_description").unwrap_or_default();
 
         let release_date = parse_release_date(release_date);
 
